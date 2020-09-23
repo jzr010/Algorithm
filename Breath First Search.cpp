fd69074816645de12a2fb8acd6e40946a6cca8c8
jzr010 <69138341+jzr010@users.noreply.github.com>
2020-09-23 15:46:28 +0900

    Update Breath First Search.cpp

--- Breath First Search.cpp
+++ Breath First Search.cpp
@@ -1,31 +1,35 @@
+//탐색을 할 때 너비를 우선으로 하여 탐색을 수행하는 탐색 알고리즘.
+//'맹목적인 탐색'
+//장점: '최단 경로', '최단 길이'를 보장 함. 
+
+
 #include <iostream>
 #include <queue>
-#include <vector>//vector 가 array보다 좋은점은 동적할당.크기를 따로 정해주지 않아도 된다/ 
+#include <vector> //메모리 할당을 위하여.
 
 using namespace std;
 
 int number = 7;
-int c[8];
-vector<int> a[8]; //8개의 백터가 들어갈 배열을 만듬.
+int c[7];//방문 처리를 위함.
+vector<int> a[8]; //자유로운 메모리 할당. 
 
 
-void bfs(int start){
-	queue<int> q;
+int bfs(int start) {
+    queue<int> q;
 	q.push(start);
-	c[start]=true;
-	while(!q.empty()){
-		int x= q.front();
+	c[start] = true;
+	while(!q.empty()) {
+		int x = q.front();
 		q.pop();
 		printf("%d",x);
-		for(int i=0;i<a[x].size();i++){//현재 q에서 꺼낸 인접한 노드. //a[x].size() 뒤에 push.back과 관련있음. 
-			int y=a[x][i];
-			if(!c[y]){
-				q.push(y);
-				c[y]=true;
-			}
-		}
-	}
-	
+		for(int i = 0; i < a[x].size(); i++) {//a[i].size()는 a와 연결된 노드의 수.
+		    int y = a[x][i];//a[x][i]는 그 start와 연결되어 있는 수.//0부터 시작하는이유는 컴퓨터에서는 숫자를 셀떄 0부터 시작. 
+		    if(!c[y]) {//그 a[x][i]를 방문하지 않았다면. 
+			    q.push(y);
+				c[y] = true; 
+		    }
+	    }	
+    }
 }
 
 int main(void){
@@ -60,6 +64,5 @@ int main(void){
 	
 	return 0; 
 	
-	
 }
-
+	
